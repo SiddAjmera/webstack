8f06b4505287b476812f24580ba02ead5b140f1a
Shilpa Lalwani <shilpal.2507@gmail.com>
2019-01-28 23:46:13 +0530

    Added animation and renamed speaker-square-card component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
+import { NgwWowService } from 'ngx-wow';
 
 @Component({
   selector: 'app-root',
@@ -40,5 +41,10 @@ export class AppComponent {
         break;
     }
   }
+
+  constructor(private wowService: NgwWowService){
+    this.wowService.init();  
+  }
+ 
 
 }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -5,8 +5,7 @@ import { AppRoutingModule } from '../app-routing/app-routing.module';
 import { HeadingComponent } from './components/heading/heading.component';
 import { EventCountdownComponent } from './components/event-countdown/event-countdown.component';
 import { AboutIntroCardComponent } from './components/about-intro-card/about-intro-card.component';
-import { SpeakerCardSquareComponent } from './components/speaker-card-square/speaker-card-square.component';
-import { SpeakerCardCircleComponent } from './components/speaker-card-circle/speaker-card-circle.component';
+import { SpeakerCardComponent } from './components/speaker-card/speaker-card.component';
 import { ScheduleTimelineComponent } from './components/schedule-timeline/schedule-timeline.component';
 import { ScheduleTimelineItemComponent } from './components/schedule-timeline-item/schedule-timeline-item.component';
 import { NewsCardComponent } from './components/news-card/news-card.component';
@@ -19,7 +18,7 @@ import { HeroComponent } from './components/hero/hero.component';
 import { DirectionComponent } from './components/direction/direction.component';
 import { DirectionTabContentComponent } from './components/direction-tab-content/direction-tab-content.component';
 import { RegisterButtonComponent } from './components/register-button/register-button.component';
-import {SpeakerModalComponent } from './components/speaker-modal/speaker-modal.component';
+import { SpeakerModalComponent } from './components/speaker-modal/speaker-modal.component';
 import { VenueComponent } from './components/venue/venue.component';
 import { TimeComponent } from './components/time/time.component';
 import { GetThereComponent } from './components/get-there/get-there.component';
@@ -35,8 +34,7 @@ import { FunFactComponent } from './components/fun-fact/fun-fact.component';
     HeadingComponent,
     EventCountdownComponent,
     AboutIntroCardComponent,
-    SpeakerCardSquareComponent,
-    SpeakerCardCircleComponent,
+    SpeakerCardComponent,
     ScheduleTimelineComponent,
     ScheduleTimelineItemComponent,
     NewsCardComponent,
@@ -60,8 +58,7 @@ import { FunFactComponent } from './components/fun-fact/fun-fact.component';
     AppRoutingModule,
     HeadingComponent,
     AboutSectionComponent,
-    SpeakerCardSquareComponent,
-    SpeakerCardCircleComponent,
+    SpeakerCardComponent,
     ScheduleTimelineComponent,
     NewsComponent,
     NewsCardComponent,
