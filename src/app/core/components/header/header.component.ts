4d7a40da5d99dbca85a599791fede627d78f08c8
--global <--global>
2019-03-25 15:52:20 +0530

    Added Event Links on the Header

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -1,16 +1,16 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
-import { DataService } from 'src/app/services/data/data.service';
-import { Observable } from 'rxjs';
+import { Component, EventEmitter, OnInit, Output } from "@angular/core";
+import { DataService } from "src/app/services/data/data.service";
+import { Observable } from "rxjs";
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  selector: "app-header",
+  templateUrl: "./header.component.html",
+  styleUrls: ["./header.component.css"]
 })
 export class HeaderComponent implements OnInit {
-
-  activeSection = 'home';
+  activeSection = "home";
   registerLink$: Observable<string>;
+  eventLink$: Observable<any[]>;
 
   @Output() sectionClick: EventEmitter<string> = new EventEmitter<string>();
 
@@ -18,11 +18,11 @@ export class HeaderComponent implements OnInit {
 
   ngOnInit() {
     this.registerLink$ = this.dataService.getRegisterLink();
+    this.eventLink$ = this.dataService.getMeetups();
   }
 
-  onSectionClick (sectionName) {
+  onSectionClick(sectionName) {
     this.activeSection = sectionName;
     this.sectionClick.emit(sectionName);
   }
-
 }
